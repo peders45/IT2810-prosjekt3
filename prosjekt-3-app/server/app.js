fd53efcb9e5ffac2df1e73e72d1c3bf5fa33af64
Jonathan <hermansenjonathan@gmail.com>
2019-10-08 18:32:50 +0200

    Refactoring ref #9

--- prosjekt-3-app/server/app.js
+++ prosjekt-3-app/server/app.js
@@ -3,10 +3,10 @@ const morgan = require ('morgan');
 const app = express();
 const mongoose = require('mongoose');
 const graphqlHttp = require('express-graphql');
-const { buildSchema } = require('graphql');
 
-const Menu = require('./api/models/menu');
-const Review = require('./api/models/review');
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
+
 
 
 const url = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}?authSource=admin`;
@@ -34,120 +34,8 @@ app.use((req, res, next)=>{
 
 
 app.use('/graphql', graphqlHttp({
-    schema: buildSchema(`
-
-        type Menu {
-            _id: ID!
-            Category: String
-            Item: String
-            Serving_Size: String
-            Calories: Int
-            Calories_from_Fat: Int
-            Total_Fat: Int
-            Total_Fat_percentage_Daily_Value: Int
-            Saturated_Fat: Int
-            Saturated_Fat_percentage_Daily_Value: Int
-            Trans_Fat: Int
-            Cholesterol: Int
-            Cholesterol_percentage_Daily_Value: Int
-            Sodium: Int
-            Sodium_percentage_Daily_Value: Int
-            Carbohydrates: Int
-            Carbohydrates_percentage_Daily_Value: Int
-            Dietary_Fiber: Int
-            Dietary_Fiber_percentage_Daily_Value: Int
-            Sugars: Int
-            Protein: Int
-            Vitamin_A_percentage_Daily_Value: Int
-            Vitamin_C_percentage_Daily_Value: Int
-            Calcium_percentage_Daily_Value: Int
-            Iron_percentage_Daily_Value: Int
-            reviews: [Review!]
-        }
-
-        type Review {
-            _id: ID!
-            name: String!
-            score: Int!
-            review: String!
-            menuItem: Menu!
-        }
-
-        type RootQuery {
-            menu: [Menu!]!
-            reviews: [Review!]!
-        }
-
-        input ReviewInput {
-            name: String!
-            score: Int!
-            review: String!
-            menuItem: ID!
-        }
-
-        type RootMutation {
-            addReview(reviewInput: ReviewInput): Review!
-        }
-
-        schema {
-            query: RootQuery
-            mutation: RootMutation
-        }
-    `),
-    rootValue: { 
-        menu: () => {
-            return Menu
-            .find()
-            .populate('reviews')
-            .then(items => {
-                return items.map( item => {
-                    return {...item._doc, _id: item.id};
-                });
-            })
-            .catch(err => {
-                throw err;
-            });
-        },
-        reviews: () => {
-            return Review
-            .find()
-            .exec()
-            .then(reviews => {
-                return reviews.map( review => {
-                    return {...review._doc, _id: review.id};
-                });
-            })
-            .catch(err => {
-                throw err;
-            });
-        },
-        addReview: (args) => {
-            const menuId = args.reviewInput.menuItem;
-            const review = new Review({
-                name: args.reviewInput.name,
-                score: args.reviewInput.score,
-                review: args.reviewInput.review,
-                menuItem: menuId
-            });
-            return Menu.findById(menuId).then( item => {
-                if(item){
-                    item.reviews.push(review);
-                    return item.save();
-                } else{
-                    throw new Error('Menu item does not exist!')
-                }
-            })
-            .then( () => {
-                return review.save()
-            }).then( result => {
-                return {...result._doc, _id: result.id};
-            })
-            .catch(err => {
-                console.log(err);
-                throw err;
-            });
-        }
-    },
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
     graphiql: true
 }));
 
---
+++ prosjekt-3-app/server/graphql/schema/index.js
@@ -0,0 +1,62 @@
+const {buildSchema} = require('graphql');
+
+module.exports = buildSchema(`
+
+type Menu {
+    _id: ID!
+    Category: String
+    Item: String
+    Serving_Size: String
+    Calories: Int
+    Calories_from_Fat: Int
+    Total_Fat: Int
+    Total_Fat_percentage_Daily_Value: Int
+    Saturated_Fat: Int
+    Saturated_Fat_percentage_Daily_Value: Int
+    Trans_Fat: Int
+    Cholesterol: Int
+    Cholesterol_percentage_Daily_Value: Int
+    Sodium: Int
+    Sodium_percentage_Daily_Value: Int
+    Carbohydrates: Int
+    Carbohydrates_percentage_Daily_Value: Int
+    Dietary_Fiber: Int
+    Dietary_Fiber_percentage_Daily_Value: Int
+    Sugars: Int
+    Protein: Int
+    Vitamin_A_percentage_Daily_Value: Int
+    Vitamin_C_percentage_Daily_Value: Int
+    Calcium_percentage_Daily_Value: Int
+    Iron_percentage_Daily_Value: Int
+    reviews: [Review!]
+}
+
+type Review {
+    _id: ID!
+    name: String!
+    score: Int!
+    review: String!
+    menuItem: Menu!
+}
+
+type RootQuery {
+    menu: [Menu!]!
+    reviews: [Review!]!
+}
+
+input ReviewInput {
+    name: String!
+    score: Int!
+    review: String!
+    menuItem: ID!
+}
+
+type RootMutation {
+    addReview(reviewInput: ReviewInput!): Review!
+}
+
+schema {
+    query: RootQuery
+    mutation: RootMutation
+}
+`)

1a99abb575ece02dfb8773013cdf9b53b234e956
Kristoffer <peders92@hotmail.com>
2019-10-17 22:52:54 +0200

    #4 Fix http header error

--- prosjekt-3-app/server/app.js
+++ prosjekt-3-app/server/app.js
@@ -18,7 +18,7 @@ app.use((req, res, next)=>{
     res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept, Authorization');
     if(req.method === 'OPTIONS'){
-        return res.sendStatus(200).json();
+        return res.sendStatus(200);
     }
     next();
 

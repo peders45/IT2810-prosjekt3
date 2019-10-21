e6b5584af9fad0dc0d97f2a83270d1ae38ecf660
Kristoffer <peders92@hotmail.com>
2019-10-21 15:35:04 +0200

    #4 Add support for update state from query

--- prosjekt-3-app/frontend/src/state/actions/searchActions.js
+++ prosjekt-3-app/frontend/src/state/actions/searchActions.js
@@ -1,23 +1,47 @@
 import client from '../../setupApolloClient';
-import gql from 'graphql-tag';	
+import gql from 'graphql-tag';
+
+const MENU_REQUESTED = "MENU_REQUESTED";
+const MENU_RECEIVED = "MENU_RECEIVED";
+const MENU_FAILED = "MENU_FAILED";
+
 
 const GET_MENU = gql`
-    query{
-      menu(categories:["Salads"]){
-        Category
-        Calories
-      }
+  query ($searchWord: String!) {
+    menu(searchWord:$searchWord){
+      Item,
+      Category,
+      Serving_Size,
+      Calories,
+      Carbohydrates,
+      Sugars
+      Protein
+      score
     }
-  `;
-
-  client.query({
-    query: GET_MENU,
-  })
-  .then(console.log);
+  }
+`;
 
 export function searchForItem(searchWord) {
-  return  {
-    type: "SEARCH",
-    payload: searchWord
-  };
-};
+    return function(dispatch) {
+      dispatch({
+        type: MENU_REQUESTED,
+      })
+  
+      client.query({
+        query: GET_MENU,
+        variables: {
+        searchWord: searchWord
+        }
+      })
+  
+      .then(data => dispatch({
+        type: MENU_RECEIVED,
+        payload: data
+      }))
+  
+      .catch(error => dispatch({
+        type: MENU_FAILED,
+        payload: error
+      }))
+    }
+  }

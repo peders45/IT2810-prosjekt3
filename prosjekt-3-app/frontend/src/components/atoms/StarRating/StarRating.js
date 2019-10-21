3dc679702aa81a7fc95f0c9460acb19e1e262310
Kristoffer <peders92@hotmail.com>
2019-10-21 21:54:39 +0200

    #14 Add action and reducer

--- prosjekt-3-app/frontend/src/components/atoms/StarRating/StarRating.js
+++ prosjekt-3-app/frontend/src/components/atoms/StarRating/StarRating.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import './StarRating.css';
 import '../../../App'
+import { rateMenu }  from "../../../state/actions/ratingActions";
+import ratingReducer  from "../../../state/reducers/ratingReducer";
+import { connect } from 'react-redux'
 import StarRatingComponent from 'react-star-rating-component';
 
 const StarRating = ({rateMenu}) => {    
     return(
       <h2>
         <StarRatingComponent 
-          name="rate1"
-          emptyStarColor={"#b7aeae"}
+          name="starsMenu"
+          emptyStarColor="#aaa2a2"
           starCount={5}
-          value={1}
+          value={ratingReducer.rateValue}
+          onStarClick={(value) => rateMenu(value)}
         />
       </h2>
     );
 };
 
-export default StarRating
+const mapDispatchToProps = {
+  rateMenu
+};
+
+const mapStateToProps = (state) => ({
+  ratingReducer
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(StarRating)

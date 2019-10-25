ea2d9f9b5760ada0651580fffdfae70150df4218
Kristoffer <peders92@hotmail.com>
2019-10-25 10:09:27 +0200

    #22 Add search with enter key

--- prosjekt-3-app/frontend/src/components/atoms/SearchField/SearchField.js
+++ prosjekt-3-app/frontend/src/components/atoms/SearchField/SearchField.js
@@ -6,6 +6,11 @@ import { connect } from 'react-redux'
 
 //Input field and button for text search
 const SearchField = ({ searchForItem }) => {
+  const handleKeyPressEnter = (event) => {
+    if(event.key === "Enter"){
+      searchForItem(input);
+    }
+  }
   const [ input, setInput ] = useState([]);
     return(
         <div className="searchField">
@@ -15,6 +20,7 @@ const SearchField = ({ searchForItem }) => {
           type="text"
           placeholder="Search for menu items..."
           onChange={(event) => setInput(event.target.value)}
+          onKeyPress={(event) => handleKeyPressEnter(event)}
           />
           <input type="button" className="searchButton" onClick={() => searchForItem(input)} value="Search"/>
         </div>

249275874958b81029aebcc412f9c2cb7b8d9b1e
Kristoffer <peders92@hotmail.com>
2019-10-25 23:51:55 +0200

    #10 unit testing

--- prosjekt-3-app/frontend/src/App.test.js
+++ prosjekt-3-app/frontend/src/App.test.js
@@ -1,9 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import configureStore from 'redux-mock-store'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const middlewares = [thunk]
+const mockStore = configureStore(middlewares)
+
+describe('Rendering', () => {
+  it('renders without crashing', () => {
+    const initialState = { 
+      searchWord: "",
+      menus: [],
+      category: [],
+      sliderMaxCalories: 1880,
+      sliderRating: 0,
+      sortCategory: "",
+      sortValue: null,
+      status: "",
+      first: 9,
+      offset: 0,
+      count: 0,
+      error: []
+    }
+    const store = mockStore(initialState)
+
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>, div);
+  });
+
+  it('renders correctly with no props', () => {
+    const component = shallow(<App/>);
+    expect(component).toMatchSnapshot();
+  });
 });
